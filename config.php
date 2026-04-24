0f48f362195ff4869042c109fe07038c0a5a5dea
ousnanii <ibikunleousnan918@gmail.com>
2026-04-24 19:20:42 +0100

    base de donnees

--- config.php
+++ config.php
@@ -1,1 +1,3 @@
-"" 
+<?php
+	$db=new PDO('mysql:host=localhost;dbname=gestion_etudiants','root','');
+?>
--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-""
+<?php 
+require 'config.php';
+if(isset($_POST['btn'])){
+	$db->query('INSERT INTO etudiants(nom,prenom,id_filieres) VALUES("'.$_POST['nom'].'","'.$_POST['prenom'].'","'.$_POST['filieres'].'")');
+}
+
+?>
+
+
+
+<form methode="POST" action="index.php">
+	<input type="name" name="nom" placeholder="nom" /><br />
+	<input type="name" name="prenom" placeholder="prenom" /><br />
+	<select name="filieres">
+		<option value = 'sil'</option>
+		<option value = 'rit'</option>
+		<option value = 'si'</option>
+	</select>
+	<input type submit="submit" name="btn" value="ajout" />
+</form>
+
+
