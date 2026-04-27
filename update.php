96fa2f42b4a38c19d5e3cde4a98775d7b28502f6
ousnanii <ibikunleousnan918@gmail.com>
2026-04-27 11:54:03 +0100

    .

--- update.php
+++ update.php
@@ -1,1 +1,17 @@
-""
+<?php
+require 'config.php';
+
+if(isset($_POST['btn'])){
+
+    $id = $_GET['id'];
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $filiere_id = $_POST['filiere_id'];
+
+    $db->query("UPDATE etudiants 
+                SET nom='$nom', prenom='$prenom', filiere_id='$filiere_id' 
+                WHERE id='$id'");
+
+    header("Location: index.php");
+}
+?>
