f2677eede841c620d4becbc957cf2b919b01c7f7
ousnanii <ibikunleousnan918@gmail.com>
2026-04-27 11:49:57 +0100

    .

--- traitement.php
+++ traitement.php
@@ -1,1 +1,7 @@
-""
+<?php
+	require 'config.php';
+	if(isset($_POST['btn'])){
+		$db->query("INSERT INTO etudiants (nom, prenom, filiere_id) VALUES('".$_POST['nom']."', '".$_POST['prenom']."', '".$_POST['filiere_id']."')");
+		header("Location: index.php");
+	}
+?>
